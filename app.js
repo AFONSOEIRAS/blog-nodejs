ea2429a5a4ec8e1131f23231ae0bf77516d0b05d
AFONSOEIRAS <afonsoeiras26@gmail.com>
2021-07-03 17:59:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const expresss = require("express")
+const handlebars= require("express-handlebars")
+const app = express()
+const mongoose = require("mongoose")
+//Configurações
+	//Body Parser
+	
+	//Handlebars
+
+	//Mongoose
+		//
+	//
+//Rotas
+
+//Outros
+
+const PORT = 8081
+app.listen(PORT,() => { 
+	console.log("Servidor rodando! ")
+})
