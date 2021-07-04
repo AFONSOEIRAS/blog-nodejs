b4909adea95fb3ded85dfd9a52366e4038eb11d4
AFONSOEIRAS <afonsoeiras26@gmail.com>
2021-07-04 17:06:09 -0300

    organizando as pastas do projeto e adicionando um grupo de rotas

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const expresss = require("express")
-const handlebars= require("express-handlebars")
+/**
+ * npm install --save express
+
+npm install --save express-handlebars
+
+npm install --save mongoose */
+const express = require("express")
+const handlebars = require("express-handlebars")
 const app = express()
 const mongoose = require("mongoose")
+const admin = require("./routes/admin")
 //Configurações
-	//Body Parser
-	
+    //Utilizando o express no lougar do Body Parser
+    app.use(express.urlencoded({extended:true}))
+    app.use(express.json())
 	//Handlebars
-
+	app.engine('handlebars', handlebars({defaultLayout: 'main'  }))
+    app.set('view engine', 'handlebars');
 	//Mongoose
-		//
+		//Em Breve
 	//
 //Rotas
-
+	app.use("/admin", admin);
 //Outros
 
 const PORT = 8081
 app.listen(PORT,() => { 
-	console.log("Servidor rodando! ")
+	console.log("Servidor rodando na url http://localhost:8081")
 })
