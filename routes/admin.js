e6651216665bef0fd139e537df42a2c08c88d4ba
AFONSOEIRAS <afonsoeiras26@gmail.com>
2021-07-06 19:51:18 -0300

    adicionando a edicao de categorias

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const { Router } = require("express")
 const express = require("express")
 const router = express.Router()
 const mongoose = require("mongoose")
@@ -52,8 +53,37 @@ router.post("/categorias/nova", (req, res) => {
             res.redirect("/admin")
         })
     }
+})
+
+router.get("/categorias/edit/:id", (req, res) => {
+    Categoria.findOne({_id:req.params.id}).lean().then((categoria)=>{
+        res.render('admin/editcategorias', {categoria:categoria})
+    }).catch((err)=> {
+        req.flash("error_msg", "Esta categoria não existe")
+        res.redirect("/admin/categorias")
+    }) 
+})
+
+router.post("/categorias/edit", (req,res) => {
+   
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(() => {
+            req.flash("success_msg", "Categoria editada com sucesso!")
+            res.redirect("/admin/categorias")
+        }).catch((err) =>{
+            req.flash("error_msg", "Houve um erro interno ao salvar a edição da categoria!")
+            res.redirect("/admin/categorias")  
+        })
 
 
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao editar a categoria")
+        res.redirect("/admin/categorias")
+    })
 })
 
 module.exports = router
